8f1ad148a6188679358bbc1f517a69d3e6729feb
pras-ops <prashantjacob.bilaspur@gmail.com>
2023-07-04 11:15:37 +0530

    data ingestion complete

---
+++ main.py
@@ -0,0 +1,13 @@
+from Chicken import logger
+from Chicken.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
