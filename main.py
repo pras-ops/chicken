dd35c0bc147befff20473bbf55fe71ce8612fa28
pras-ops <prashantjacob.bilaspur@gmail.com>
2023-07-07 10:18:24 +0530

    Dvc added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from Chicken import logger
 from Chicken.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Chicken.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from Chicken.pipeline.stage_03_training import ModelTrainingPipeline
+from Chicken.pipeline.stage_04_evaluation import EvaluationPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -35,3 +37,13 @@ except Exception as e:
     logger.exception(e)
     raise e
  
+STAGE_NAME = "Evaluation stage"
+try:
+    logger.info(f"******************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj =EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
