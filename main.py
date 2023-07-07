3c5ea36ef36ca1888803905cf454aa616a49646c
pras-ops <prashantjacob.bilaspur@gmail.com>
2023-07-07 08:41:34 +0530

    training added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Chicken import logger
 from Chicken.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Chicken.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from Chicken.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -23,3 +23,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"******************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+ 
